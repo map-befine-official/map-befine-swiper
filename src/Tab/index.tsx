b4809f75456e94e76475241ab4eed1e13d114136
semnil5202 <semnil5202@gmail.com>
2024-07-11 17:26:42 +0900

    chore: chromatic 배포 및 버전 업그레이드

--- src/Tab/index.tsx
+++ src/Tab/index.tsx
@@ -1,8 +1,8 @@
 import React, { HTMLAttributes } from 'react';
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
-  label: string;
-  children?: React.ReactNode;
+  label?: string;
+  children: React.ReactNode;
 }
 
 function Tab({ children, ...attribute }: Props) {
